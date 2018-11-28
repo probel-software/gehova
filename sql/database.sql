0451af0b50d3e5e317183ab20589e38a99f35f43
jibedoubleve <jibedoubleve@gmail.com>
2018-11-28 21:49:30 +0100

    WIP - add views

--- sql/database.sql
+++ sql/database.sql
@@ -2,18 +2,18 @@ create table if not exists pickup_round (
     id   integer primary key,
     name text
 );
-
+-------------------------------------------------------------------------------
 create table if not exists team (
     id integer primary key,
     name text
 );
-
+-------------------------------------------------------------------------------
 create table if not exists category (
-    id integer primary key,
+    id      integer primary key,
     key     text,
     display text
 );
-
+-------------------------------------------------------------------------------
 create table if not exists person (
     id integer primary key,
     first_name           text,
@@ -26,7 +26,7 @@ create table if not exists person (
     foreign key(pickup_round_id) references pickup_round(id),
     foreign key(team_id)         references team(id) 
 );
-
+-------------------------------------------------------------------------------
 create table if not exists absence (
     id         integer primary key,
     date_start date,
@@ -34,10 +34,10 @@ create table if not exists absence (
     person_id  integer,
     foreign key(person_id) references person(id)
 );
-
+-------------------------------------------------------------------------------
 create table if not exists person_category (
     person_id   integer,
     category_id integer,
     foreign key(person_id)   references person(id),
     foreign key(category_id) references category(id)
-)
+);
--- sql/views/views_absences.sql
+++ sql/views/views_absences.sql
@@ -1,6 +1,51 @@
 drop view if exists absence_today_v;
 create view absence_today_v as 
-    select * 
-    from absence
+    select a.date_start                       as date_start
+         , a.date_end                         as date_end
+         , p.first_name || ' ' || p.last_name as person_name
+         , t.name                             as team
+    from absence a
+    inner join person p on a.person_id = p.id
+    inner join team t   on p.team_id = t.id
     where date('now') > date_start 
       and date('now') < date_end;
+-------------------------------------------------------------------------------
+drop view if exists all_absence_v;
+create view all_absence_v as 
+    select a.date_start                       as date_start
+         , a.date_end                         as date_end
+         , p.first_name || ' ' || p.last_name as person_name
+         , t.name                             as team
+         , is_lunchtime
+         , is_reception_evening
+         , is_reception_morning
+    from absence a
+    inner join person p on a.person_id = p.id
+    inner join team t   on p.team_id = t.id;
+-------------------------------------------------------------------------------
+drop view if exists absence_lunchtime_v;
+create view absence_lunchtime_v as 
+    select date_start
+         , date_end
+         , person_name
+         , team
+      from all_absence_v
+    where is_lunchtime = 1;
+-------------------------------------------------------------------------------
+drop view if exists absence_reception_morning_v;
+create view absence_reception_morning_v as     
+        select date_start
+         , date_end
+         , person_name
+         , team
+     from all_absence_v
+    where is_reception_morning = 1;
+-------------------------------------------------------------------------------
+drop view if exists absence_reception_evening_v;
+create view absence_reception_evening_v as 
+        select date_start
+         , date_end
+         , person_name
+         , team
+     from all_absence_v
+    where is_reception_evening = 1;
