6d72cf779262601143abac7cc62e6c81999f0acc
jibedoubleve <jibedoubleve@users.noreply.github.com>
2019-01-29 14:32:05 +0100

    Fixed SQL script to import debug data(fix #17)

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -30,32 +30,32 @@ insert into pickup_round(id, name) values (4, 'RD');
 --------------------------------------
 -- PERSON
 --------------------------------------
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (1, 'Robert' , 'Dupont'   , 0, 1, 0, 1, 1);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (2, 'Durant' , 'Luc'      , 0, 1, 1, 0, 1);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (3, 'Clause' , 'Zorg'     , 0, 1, 0, 1, 2);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (4, 'Laurent', 'Vandeput' , 0, 1, 1, 1, 2);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (5, 'Mario'  , 'Bross'    , 0, 1, 0, 1, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (6, 'Mac', 'Winston', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (7, 'Jordon', 'Mcguinness', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (8, 'Starr', 'Echavarria', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (9, 'Yael', 'Chastain', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (10, 'Hugo', 'Engelhard', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (11, 'Ariel', 'Wetherbee', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (12, 'Asha', 'Heiss', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (13, 'Cleotilde', 'Manzer', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (14, 'Albina', 'Kindred', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (15, 'Tressie', 'Francese', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (16, 'Destiny', 'Kincade', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (17, 'Beau', 'Hedgecock', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (18, 'Kathyrn', 'Gaffney', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (19, 'Dorie', 'Angulo', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (20, 'Vilma', 'Rentfro', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (21, 'Leoma', 'Marth', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (22, 'Christiana', 'Wison', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (23, 'Jeannine', 'Pullin', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (24, 'Nan', 'Marchese', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (25, 'Glynda', 'Applin', 0, 1, 1, 0, 3);
-insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (26, 'Luke'   , 'Skywalker', 0, 1, 1, 0, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (1, 'Robert'     , 'Dupont'    , 0, 1, 0, 1, 1);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (2, 'Durant'     , 'Luc'       , 0, 1, 1, 2, 1);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (3, 'Clause'     , 'Zorg'      , 0, 1, 0, 1, 2);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (4, 'Laurent'    , 'Vandeput'  , 0, 1, 1, 1, 2);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (5, 'Mario'      , 'Bross'     , 0, 1, 0, 1, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (6, 'Mac'        , 'Winston'   , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (7, 'Jordon'     , 'Mcguinness', 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (8, 'Starr'      , 'Echavarria', 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (9, 'Yael'       , 'Chastain'  , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (10, 'Hugo'      , 'Engelhard' , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (11, 'Ariel'     , 'Wetherbee' , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (12, 'Asha'      , 'Heiss'     , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (13, 'Cleotilde' , 'Manzer'    , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (14, 'Albina'    , 'Kindred'   , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (15, 'Tressie'   , 'Francese'  , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (16, 'Destiny'   , 'Kincade'   , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (17, 'Beau'      , 'Hedgecock' , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (18, 'Kathyrn'   , 'Gaffney'   , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (19, 'Dorie'     , 'Angulo'    , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (20, 'Vilma'     , 'Rentfro'   , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (21, 'Leoma'     , 'Marth'     , 0, 1, 1, 2, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (22, 'Christiana', 'Wison'     , 0, 1, 1, 1, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (23, 'Jeannine'  , 'Pullin'    , 0, 1, 1, 1, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (24, 'Nan'       , 'Marchese'  , 0, 1, 1, 1, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (25, 'Glynda'    , 'Applin'    , 0, 1, 1, 1, 3);
+insert into person(id, first_name, last_name, is_lunchtime, is_reception_morning, is_reception_evening, pickup_round_id, team_id) values (26, 'Luke'      , 'Skywalker' , 0, 1, 1, 1, 3);
 
 --------------------------------------
 -- ABSENCE
